c57e1fdf899e6ee5bf02a0dd1decfd36c8d965e6
sergey <sergey_pidd@ukr.net>
2020-08-21 23:22:04 +0300

    add canvas

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const showRulesBtn = document.querySelector("#rules-btn");
 const closeRulesBtn = document.querySelector("#close-btn");
 const rules = document.querySelector("#rules");
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
 const showRules = () => {
   rules.classList.add("show");
